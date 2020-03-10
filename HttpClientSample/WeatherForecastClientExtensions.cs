592677d2d0d97e70632e25b0f6227a591075f802
William Guo <william.guo@plexure.com>
2020-03-10 22:39:37 +1300

    Add polly

--- HttpClientSample/WeatherForecastClientExtensions.cs
+++ HttpClientSample/WeatherForecastClientExtensions.cs
@@ -1,17 +1,39 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
 
 namespace HttpClientSample
 {
     public static class WeatherForecastClientExtensions
     {
+        private static readonly Random Jitterer = new Random();
+        private const int RetryCount = 5;
+
         public static IHttpClientBuilder AddWeatherForecastClient(this IServiceCollection services, Uri baseAddress)
         {
-            return services.AddHttpClient<IWeatherForecastClient, WeatherForecastClient>(httpClient =>
-            {
-                httpClient.BaseAddress = baseAddress;
-                httpClient.DefaultRequestHeaders.Add("Weather-Forecast-Api-Key-Name", "WeatherForecastApiKeyValue");
-            });
+            return services
+                .AddHttpClient<IWeatherForecastClient, WeatherForecastClient>(httpClient =>
+                {
+                    httpClient.BaseAddress = baseAddress;
+                    httpClient.DefaultRequestHeaders.Add("Weather-Forecast-Api-Key-Name", "WeatherForecastApiKeyValue");
+                })
+                .AddTransientHttpErrorPolicy(p =>
+                    p.WaitAndRetryAsync(
+                        RetryCount,
+                        currentRetryNumber => TimeSpan.FromSeconds(Math.Pow(2, currentRetryNumber)) + TimeSpan.FromMilliseconds(Jitterer.Next(0, 100)),
+                        (currentException, currentSleepDuration, currentRetryNumber, currentContext) => 
+                        {
+                            if (currentRetryNumber == RetryCount)
+                            {
+                                throw currentException.Exception;
+                            }
+#if DEBUG
+                            Debug.WriteLine($"=== Attempt {currentRetryNumber} ===");
+                            Debug.WriteLine(nameof(currentException) + ": " + currentException.Exception);
+                            Debug.WriteLine(nameof(currentSleepDuration) + ": " + currentSleepDuration);
+#endif
+                        }));
         }
     }
 }
